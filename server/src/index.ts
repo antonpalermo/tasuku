366b2e9050603bf154d4ee29b618c8ca04aa13ff
Anton Palermo <ANPBusinessmail@gmail.com>
2021-12-10 22:56:22 +0800

    swap apollo-server to apollo-server-express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,27 @@
 import 'reflect-metadata'
 
+import express from 'express'
+
 import { buildSchema } from 'type-graphql'
-import { ApolloServer } from 'apollo-server'
+import { ApolloServer } from 'apollo-server-express'
 
 const main = async () => {
+  const app = express()
+  const port = process.env.PORT || 4000
+
   const apollo = new ApolloServer({
     schema: await buildSchema({
       resolvers: [`${__dirname}/resolvers/**/*.ts`],
     }),
   })
 
-  apollo.listen().then(({ url }) => console.log(`server started on ${url}`))
+  await apollo.start()
+
+  apollo.applyMiddleware({ app, path: '/' })
+
+  app.listen(port, () =>
+    console.log(`Server started on http://localhost:${port}`)
+  )
 }
 
 main().catch((e) => console.log(e))
