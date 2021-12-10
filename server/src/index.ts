2e63798a1d68a2e5b7d1e6ac91958af9dddaf9b6
Anton Palermo <ANPBusinessmail@gmail.com>
2021-12-10 23:32:23 +0800

    added websocket pubsub support

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,27 +1,57 @@
 import 'reflect-metadata'
 
+import http from 'http'
 import express from 'express'
 
 import { buildSchema } from 'type-graphql'
+import { execute, subscribe } from 'graphql'
 import { ApolloServer } from 'apollo-server-express'
+import { SubscriptionServer } from 'subscriptions-transport-ws'
 
 const main = async () => {
   const app = express()
+  const httpServer = http.createServer(app)
+
   const port = process.env.PORT || 4000
 
-  const apollo = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [`${__dirname}/resolvers/**/*.ts`],
-    }),
+  const schema = await buildSchema({
+    resolvers: [`${__dirname}/resolvers/**/*.ts`],
+    validate: false,
   })
+
+  const apollo = new ApolloServer({
+    schema,
+    plugins: [
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              ws.close()
+            },
+          }
+        },
+      },
+    ],
+  })
+
+  const ws = SubscriptionServer.create(
+    {
+      schema,
+      execute,
+      subscribe,
+    },
+    {
+      server: httpServer,
+      path: '/',
+    }
+  )
 
   await apollo.start()
 
   apollo.applyMiddleware({ app, path: '/' })
 
-  app.listen(port, () =>
-    console.log(`Server started on http://localhost:${port}`)
-  )
+  await new Promise<void>((res) => httpServer.listen({ port }, res))
+  console.log(`🚀 Server ready at http://localhost:4000${apollo.graphqlPath}`)
 }
 
 main().catch((e) => console.log(e))
