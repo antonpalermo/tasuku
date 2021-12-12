9e0965ad3fa94841f5af1b411a4d423790379845
Anton Palermo <ANPBusinessmail@gmail.com>
2021-12-13 00:32:02 +0800

    init apollo client

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -2,10 +2,54 @@ import { AppProps } from 'next/app'
 
 import Layout from '../components/Layout'
 
+import { WebSocketLink } from '@apollo/client/link/ws'
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+  split,
+} from '@apollo/client'
+import { getMainDefinition } from '@apollo/client/utilities'
+
+const wsLink = process.browser
+  ? new WebSocketLink({
+      uri: `ws://localhost:4000`,
+      options: {
+        reconnect: true,
+      },
+    })
+  : new HttpLink()
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:4000',
+})
+
+const link = process.browser
+  ? split(
+      ({ query }) => {
+        const definition = getMainDefinition(query)
+        return (
+          definition.kind === 'OperationDefinition' &&
+          definition.operation === 'subscription'
+        )
+      },
+      wsLink,
+      httpLink
+    )
+  : httpLink
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+})
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
-      <Component {...pageProps} />
+      <ApolloProvider client={client}>
+        <Component {...pageProps} />
+      </ApolloProvider>
     </Layout>
   )
 }
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,5 +1,53 @@
+import { gql, useQuery, useSubscription } from '@apollo/client'
+
+const FETCH_TASKS = gql`
+  query Query {
+    tasks {
+      name
+      id
+      isComplete
+      dateCreated
+      dateUpdated
+    }
+  }
+`
+
+const ON_TASK_CREATED_SUB = gql`
+  subscription Subscription {
+    on {
+      id
+      name
+      isComplete
+      dateCreated
+      dateUpdated
+    }
+  }
+`
+
 const Home = () => {
-  return <h1>Home page</h1>
+  const { loading, data: task, error } = useQuery(FETCH_TASKS)
+  const { loading: taskLoading, data } = useSubscription(ON_TASK_CREATED_SUB)
+  if (loading) {
+    return <h1>Loading page...</h1>
+  }
+
+  if (error) {
+    return <h1>Error page!</h1>
+  }
+
+  console.log(data)
+
+  return (
+    <div>
+      {task.tasks.map((x: any) => (
+        <div key={x.id}>
+          <h1>{x.name}</h1>
+        </div>
+      ))}
+
+      <h1>New Task: {!taskLoading && data?.on.name}</h1>
+    </div>
+  )
 }
 
 export default Home
