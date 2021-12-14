70444b2455005ce12eef1d0faa26253d7d321ea0
Anton Palermo <ANPBusinessmail@gmail.com>
2021-12-14 21:31:58 +0800

    add subcription

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,51 +1,58 @@
 import { gql, useQuery, useSubscription } from '@apollo/client'
-
-const FETCH_TASKS = gql`
-  query Query {
-    tasks {
-      name
-      id
-      isComplete
-      dateCreated
-      dateUpdated
-    }
-  }
-`
-
-const ON_TASK_CREATED_SUB = gql`
-  subscription Subscription {
-    on {
-      id
-      name
-      isComplete
-      dateCreated
-      dateUpdated
-    }
-  }
-`
+import { useEffect, useState } from 'react'
 
 const Home = () => {
-  const { loading, data: task, error } = useQuery(FETCH_TASKS)
-  const { loading: taskLoading, data } = useSubscription(ON_TASK_CREATED_SUB)
+  const FETCH_POST = gql`
+    query Tasks {
+      tasks {
+        id
+        name
+        isComplete
+        dateCreated
+        dateUpdated
+      }
+    }
+  `
+
+  const ON_CREATE = gql`
+    subscription Subscription {
+      onCreate {
+        task {
+          id
+          name
+          isComplete
+          dateCreated
+          dateUpdated
+        }
+        message
+      }
+    }
+  `
+
+  const [tasks, setTasks] = useState<any[]>()
+  const { loading } = useQuery(FETCH_POST, {
+    onCompleted: (data) => setTasks(data?.tasks),
+  })
+
+  useSubscription(ON_CREATE, {
+    onSubscriptionData: ({ subscriptionData: { data } }) =>
+      setTasks((old: any) => [...old, data?.onCreate.task]),
+  })
+
   if (loading) {
-    return <h1>Loading page...</h1>
+    return <h1>Loading...</h1>
   }
-
-  if (error) {
-    return <h1>Error page!</h1>
-  }
-
-  console.log(data)
 
   return (
     <div>
-      {task.tasks.map((x: any) => (
-        <div key={x.id}>
-          <h1>{x.name}</h1>
-        </div>
-      ))}
-
-      <h1>New Task: {!taskLoading && data?.on.name}</h1>
+      <h1>Home Components</h1>
+      <div>
+        {tasks?.map((x: any) => (
+          <div key={x.id}>
+            <h2>{x.name}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
